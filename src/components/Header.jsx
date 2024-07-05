82231bf6ae8a1ab69e771c7daa0bf06fce2d6a58
Saifullah-max <saifullahahmed380@gmail.com>
2024-07-06 00:44:04 +0500

    updated history component with new functioning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <div>
         <h1>Expense Tracker</h1>
-        <h4>YOUR BALANCE</h4>
+        <h4>YOUR BALANCE IS: </h4>
         <h3>$0.00</h3>
     </div>
   )
--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const History = ({submittedText, submittedNum, transactionNumber}) => {
+    const [history, setHistory] = useState([])
+
+    useEffect(() => {
+        if (submittedText && submittedNum) {
+            setHistory (prev => [...prev, {text: submittedText, amount: submittedNum}])
+        }
+    }, [submittedText, submittedNum])
   return (
     <div>
-        History of Transactions:
+        <div>
+        Recent Transaction History:
         <h3>Transaction {transactionNumber}:</h3>
-        <p>{submittedText}  {submittedNum}</p>
+        <p>{submittedText} {submittedNum}</p>
+        </div>
+        <div>
+            <h5>All Time Transaction History</h5>
+            <ul>
+                {history.map((item, index) => 
+                <li key={index}>
+                    {item.text} {item.amount}
+                </li>
+            )}
+            </ul>
+        </div>
     </div>
   )
 }
